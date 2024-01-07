884877c771a73654767565517fd00a14e026950a
Gani Madaris <dulab104@gmail.com>
2024-01-07 13:11:02 +0800

    fixing some bugs in js and making changes from project to language

--- src/JS/hamburger.js
+++ src/JS/hamburger.js
@@ -1,13 +1,27 @@
 const hamburger = document.getElementById("hamburger");
 const navUl = document.getElementById("ulHamb");
+let width = 0;
 console.log(navUl);
+console.log(window.innerWidth);
+window.addEventListener("resize", detectResize);
 
 hamburger.onclick = () => {
-  if (hamburger.classList.contains("active")) {
-    hamburger.classList.remove("active");
-    navUl.classList.replace("absolute", "hidden");
-  } else if (!hamburger.classList.contains("active")) {
-    hamburger.classList.add("active");
-    navUl.classList.replace("hidden", "absolute");
+  if (width <= 768) {
+    if (hamburger.classList.contains("active")) {
+      hamburger.classList.remove("active");
+      navUl.classList.replace("absolute", "hidden");
+    } else if (!hamburger.classList.contains("active")) {
+      hamburger.classList.add("active");
+      navUl.classList.replace("hidden", "absolute");
+    }
   }
 };
+
+function detectResize(e) {
+  width = e.currentTarget.innerWidth;
+  if (width > 768) {
+    hamburger.classList.remove("active");
+    navUl.classList.replace("absolute", "hidden");
+  }
+  console.log(width);
+}
